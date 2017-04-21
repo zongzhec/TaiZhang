2cc875f2a38f34e205e54b0ed73b401b4b4355c8
zongzhe <zongzhe_chen@sina.com>
2017-04-21 08:51:07 +0800

    checking file before running

--- src/foo/zongzhe/taizhang/model/StartingPoint.java
+++ src/foo/zongzhe/taizhang/model/StartingPoint.java
@@ -3,6 +3,13 @@ package foo.zongzhe.taizhang.model;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ClassGenerator;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.MethodGenerator;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.TypeCheckError;
+
 import foo.zongzhe.taizhang.common.DirectoryAction;
 import foo.zongzhe.taizhang.common.LogAction;
 import foo.zongzhe.taizhang.view.PreWelcomeView;
@@ -11,8 +18,10 @@ import foo.zongzhe.taizhang.view.WelcomeView;
 public class StartingPoint {
 
 	static String ErrorMsg;
+	static File inputDir = new File("C:/taizhang/input");
+	static File outputDir = new File("C:/taizhang/output");
+	static File logDir = new File("C:/taizhang/log");
 	public static String logFileName;
-	static File fileName;
 
 	public static void main(String[] args) {
 
@@ -24,24 +33,22 @@ public class StartingPoint {
 		// Check required directory and start to log
 		DirectoryAction da = new DirectoryAction();
 		File dir = new File("C:/taizhang/log");
-		if (!da.whetherDirectoryExists(dir)) {
+		if (!da.whetherDirectoryExists(StartingPoint.logDir)) {
 			da.createDir(dir);
 		}
 
-		dir = new File("C:/taizhang/input");
-		if (!da.whetherDirectoryExists(dir)) {
+		if (!da.whetherDirectoryExists(StartingPoint.inputDir)) {
 			da.createDir(dir);
 		}
 
-		dir = new File("C:/taizhang/output");
-		if (!da.whetherDirectoryExists(dir)) {
+		if (!da.whetherDirectoryExists(StartingPoint.outputDir)) {
 			da.createDir(dir);
 		}
 
 		// Set time format
 		SimpleDateFormat dfFileName = new SimpleDateFormat("yyyyMMdd");
 		String sysTimeFileName = dfFileName.format(new Date()).toString();
-		logFileName = "C:/taizhang/log/taizhang_" + sysTimeFileName + ".out";
+		logFileName = StartingPoint.logDir + "/taizhang_" + sysTimeFileName + ".out";
 		File logFile = new File(logFileName);
 		if (!da.whetherFileExists(logFile)) {
 			da.createFile(logFile);
@@ -70,12 +77,18 @@ public class StartingPoint {
 		while (itWorks) {
 
 			// 检查输入的文本是否存在，若不存在，则报错后退出
-			File inputText = new File("C:/taizhang/input");
-			if (!da.whetherDirectoryExists(inputText)) {
-				ErrorMsg = "Input file does not exist, exit";
-				itWorks = false;
+			String[] fileList = da.getFileList(StartingPoint.inputDir);
+			itWorks = false;
+			for (int i = 0; i < fileList.length; i++) {
+				if (fileList[i].contains("投资明细")) {
+					la.log(StartingPoint.logFileName, "Info", "file checking success. 投资明细 is included in file names.");
+				}
+			}
+			if (!itWorks) {
+				ErrorMsg = "File names does not contain 投资明细";
 				break;
 			}
+			itWorks = true;
 
 		}
 
